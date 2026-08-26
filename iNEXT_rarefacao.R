94c6db54cfd9bc640c5fc8296d34c37e273380a7
Pedro Martins <pedro.rufino.martins@gmail.com>
2026-08-26 11:18:29 -0300

    few changes

--- iNEXT_rarefacao.R
+++ iNEXT_rarefacao.R
@@ -7,16 +7,16 @@ rarefacaoRichiness = iNEXT (all_sites, q = c(0,1,2))
 R_E_sites_mixedforest_by_ha <-  ggiNEXT (rarefacaoRichiness, facet.var = "Order.q") +
   theme_minimal(base_family = "serif") +
   facet_wrap(~Order.q, scale = "free", labeller = as_labeller(c(
-    "0" = "Species richness",
-    "1" = "Shannon diversity",
-    "2" = "Simpson diversity"))) +
+    "0" = "a) \t Species richness",
+    "1" = "b) \t Shannon diversity",
+    "2" = "c) \t Simpson diversity"))) +
   labs(
     x = "Number of individuals",
     y = "Species diversity"
   ) + theme (legend.position = "bottom",
              legend.text = element_text(size = 8))
 
-ggsave ("R_E_mixed_by_ha.jpg",
+ggsave ("R_E_mixed_by_ha_tes1.jpg",
         R_E_sites_mixedforest_by_ha,
         width = 2500,
         height = 1500,
